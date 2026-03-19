fc622adf52ca5e6f962de1b61fe8ca6b358f2451
K.A <akkazu1121@gmail.com>
2026-03-19 16:40:42 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -6,35 +6,18 @@ self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim())
 })
 
-function proxify(url) {
-  try {
-    if (url.startsWith("http")) {
-      return "/proxy?url=" + encodeURIComponent(url)
-    }
-    return url
-  } catch {
-    return url
-  }
-}
-
 self.addEventListener("fetch", event => {
-
   const req = event.request
   const url = new URL(req.url)
 
-  // 自分のドメインはそのまま
-  if (url.pathname.startsWith("/proxy")) {
-    return
-  }
-
-  const target = req.url
+  // 自分のサイトは除外
+  if (url.origin === location.origin) return
 
   event.respondWith(
-    fetch(proxify(target), {
+    fetch("/proxy?url=" + encodeURIComponent(req.url), {
       method: req.method,
       headers: req.headers,
-      body: req.method !== "GET" ? req.body : undefined,
-      redirect: "follow"
+      body: req.method !== "GET" ? req.body : undefined
     }).catch(() => fetch(req))
   )
 })
